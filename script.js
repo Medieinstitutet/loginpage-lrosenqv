e241b3e1ba8583caacfeb467e526f65adc3e45e1
lrosenqv <kimle.rosenqvist@gmail.com>
2021-12-02 19:41:28 +0100

    added html + added array userName

---
+++ script.js
@@ -0,0 +1,17 @@
+// Hämta alla element
+let userNameInput = document.getElementById("userNameInput")
+let passwordInput = document.getElementById("passwordInput")
+let loginBtn = document.getElementById("loginBtn")
+let signUpBtn = document.getElementById("signUpBtn")
+
+//Array användarnamn
+let userName = ["Louise", "Janne", "Victor"]
+console.log(userName)
+
+//Array lösenord
+
+
+//Event för inlogg-knapp
+
+
+//Event för skapa ny användare-knapp
