00b5edf3bc005acecbb342c3cd76574bf0254c99
lrosenqv <kimle.rosenqvist@gmail.com>
2021-12-03 05:26:40 +0100

    added functions

--- script.js
+++ script.js
@@ -1,36 +1,89 @@
+// Array username
+let userNames = ["Louise", "Janne", "Victor"];
+
+if(userNames == null) {
+    localStorage.setItem("userNames", JSON.stringify(userNames));
+}
+
+// Array password
+let passwords = ["chihuahua", "test", "test2"];
+
+if(passwords == null) {
+    localStorage.setItem("passwords", JSON.stringify(passwords));
+}
+
+
 // Hämta alla element
 let userNameInput = document.getElementById("userNameInput")
-let signUpBtn = document.getElementById("signUpBtn")
 let passwordInput = document.getElementById("passwordInput")
 
 //Inloggningsknapp
 let loginBtn = document.getElementById("loginBtn").addEventListener("click", validateUser)
 
-// Array username
-let userNames = ["Louise", "Janne", "Victor"];
-localStorage.setItem("userNames", JSON.stringify(userNames))
+//Ny användare-knapp 
+let signUpBtn = document.getElementById("signUpBtn").addEventListener("click", addNewUser)
 
-// Array password
-let passwords = ["chihuahua", "test", "test2"];
-localStorage.setItem("passwords", JSON.stringify(passwords))
 
-console.log(userNames, passwords)
+//Hämta listor från Local storage
+function getUsersFromLS() {
+    let collectedUserList = localStorage.getItem("userNames");
+
+    let userNames = []
+
+    if(collectedUserList) {
+        userNames = JSON.parse(collectedUserList);
+        return userNames 
+    }
+}
+
+function getPassFromLS() {
+    let collectedPassList = localStorage.getItem("passwords");
+    
+    let passwords = []
+
+    if(collectedPassList) {
+        passwords = JSON.parse(collectedPassList);
+        return passwords 
+    }
+}
 
 //Inloggning och validering
 function validateUser() {
     let userName = document.getElementById("userNameInput").value
     let password = document.getElementById("passwordInput").value
     
-    for(let i = 0; i < userNames.length; i++) {
-
-        //if(userNames.indexOf(userName) === passwords.indexOf(password)) {
-        if((userName == userNames[i]) &&(password == passwords[i])) {
-        console.log("yaay")
-            break;
+    for(let i = 0; i < userNames.length; i++) { 
+        if((userName == userNames[i]) && (password == passwords[i])) {
+            console.log("yaay"); 
+            break; 
 
         } else {
-            console.log("feel")
-            break;
+
+            console.log("feel"); 
+            break;}
         }
-    }
 }
+
+//Lägg till ny användare
+function addNewUser() {
+
+    let newUser = document.getElementById("userNameInput").value
+
+    let userNames = getUsersFromLS() 
+
+    userNames.push(newUser)
+
+    localStorage.setItem("userNames", JSON.stringify(userNames))
+
+
+    //Nytt lösenord   
+    let newPass = document.getElementById("passwordInput").value
+
+    let passwords = getPassFromLS() 
+   
+    passwords.push(newPass)
+ 
+    localStorage.setItem("passwords", JSON.stringify(passwords))
+}
+
+
